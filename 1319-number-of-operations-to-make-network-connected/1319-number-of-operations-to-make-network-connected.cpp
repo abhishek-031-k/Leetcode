b1b66db34a98fbda103cd35c266ce0e0d445f2a9
Abhishek kumar <abhishkumar159@gmail.com>
2026-06-30 16:14:20 +0530

    Time: 0 ms (100%), Space: 42.4 MB (72.88%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,67 @@
+
+  class DisjointSet{
+  public:
+   vector<int>rank,parent, size;
+    DisjointSet(int n){
+    rank.resize(n+1, 0);
+    parent.resize(n+1, 0);
+    size.resize(n+1, 0);
+    for(int i = 0; i < n; i++)parent[i] = i;
+    }
+
+   int find(int node){
+    if(node == parent[node])return node;
+    return parent[node] = find(parent[node]);
+   }
+
+   void unionByRank(int u, int v){
+    int ulp_u = find(u);
+    int ulp_v = find(v);
+    if(ulp_u == ulp_v)return;
+    if(rank[ulp_u] < rank[ulp_v]){
+      parent[ulp_u] = ulp_v;
+    }
+    else if(rank[ulp_u] > rank[ulp_v]){
+       parent[ulp_v] = ulp_u;
+    }
+    else {
+        parent[ulp_v] = ulp_u;
+        rank[ulp_u]++;
+    }
+   }
+
+    void unionBySize(int u, int v){
+     int ulp_u = find(u);
+     int ulp_v = find(v);
+     if(ulp_u == ulp_v)return;
+     if(size[ulp_u] < size[ulp_v]){
+        parent[ulp_u] = ulp_v;
+        size[ulp_v] += size[ulp_u];
+     }
+     else{
+        parent[ulp_v] = ulp_u;
+        size[ulp_u] += size[ulp_v];
+     }
+    }
+
+
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int size = connections.size();
+        if(n-1 > size)return -1;
+        int extra = 0;
+        DisjointSet ds(n);
+        for(auto &it: connections){
+            if(ds.find(it[0]) == ds.find(it[1]))extra++;
+            else ds.unionBySize(it[0], it[1]);
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(ds.parent[i] == i)count++;
+        }
+        return count-1;
+    }
+};
