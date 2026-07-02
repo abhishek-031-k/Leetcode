9877ffec73c231ca1acbd803b32211e11c1f3a12
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-02 10:26:46 +0530

    Time: 18 ms (62.1%), Space: 30.5 MB (84.59%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, 
+        greater<pair<int, pair<int, int>>>>pq;
+          
+        int drow[4] = {1, 0, -1, 0};
+        int dcol[4] = {0, 1, 0, -1};
+       dist[0][0] = grid[0][0];
+       pq.push({grid[0][0], {0, 0}});
+       while(!pq.empty()){
+         auto it = pq.top();
+         pq.pop();
+         int dis = it.first;
+         int row = it.second.first;
+         int col = it.second.second;
+        if(dis > dist[row][col])continue;
+         for(int i = 0; i < 4; i++){
+            int nrow = drow[i] + row, ncol = dcol[i] + col;
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
+                 if(dist[nrow][ncol] > dis + grid[nrow][ncol]){
+                    dist[nrow][ncol] = dis + grid[nrow][ncol];
+                    pq.push({dist[nrow][ncol], {nrow, ncol}});
+                 }
+            }
+         }
+       }
+       return (dist[n-1][m-1] < health) ? true : false;
+    }
+};
