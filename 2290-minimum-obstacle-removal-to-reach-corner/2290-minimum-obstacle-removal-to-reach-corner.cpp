e65b695b3be56268a999221f529accca7a3317a8
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-02 11:10:36 +0530

    Time: 226 ms (60.2%), Space: 108.7 MB (79.6%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = grid[0][0];
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+        greater<pair<int, pair<int, int>>>>pq;
+        pq.push({grid[0][0], {0, 0}});
+
+       int drow[4] = {1, 0, -1, 0};
+       int dcol[4] = {0, 1, 0, -1};
+
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int dis = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            for(int i = 0; i < 4; i++){
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
+                    if(dist[nrow][ncol] > dis + grid[nrow][ncol]){
+                        dist[nrow][ncol] = dis + grid[nrow][ncol];
+                        pq.push({dist[nrow][ncol], {nrow, ncol}});
+                    }
+                }
+            }
+        }
+        return dist[n-1][m-1];
+    }
+};
