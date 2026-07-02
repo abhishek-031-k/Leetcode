8f7849413d9c9ebb72f1eaa4b0c24ec806ad83fd
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-02 11:19:04 +0530

    Time: 117 ms (76.15%), Space: 129.3 MB (34.72%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -5,17 +5,15 @@ public:
         int m = grid[0].size();
         vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
         dist[0][0] = grid[0][0];
-
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
-        pq.push({grid[0][0], {0, 0}});
+        deque<pair<int, pair<int, int>>>dq;
+        dq.push_front({grid[0][0], {0, 0}});
 
        int drow[4] = {1, 0, -1, 0};
        int dcol[4] = {0, 1, 0, -1};
 
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
+        while(!dq.empty()){
+            auto it = dq.front();
+            dq.pop_front();
             int dis = it.first;
             int row = it.second.first;
             int col = it.second.second;
@@ -24,9 +22,10 @@ public:
                 int nrow = drow[i] + row;
                 int ncol = dcol[i] + col;
                 if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
-                    if(dist[nrow][ncol] > dis + grid[nrow][ncol]){
+                      if (dist[nrow][ncol] > dis + grid[nrow][ncol]) {
                         dist[nrow][ncol] = dis + grid[nrow][ncol];
-                        pq.push({dist[nrow][ncol], {nrow, ncol}});
+                        if (grid[nrow][ncol] == 0)dq.push_front({dist[nrow][ncol], {nrow, ncol}});
+                        else dq.push_back({dist[nrow][ncol], {nrow, ncol}});
                     }
                 }
             }
