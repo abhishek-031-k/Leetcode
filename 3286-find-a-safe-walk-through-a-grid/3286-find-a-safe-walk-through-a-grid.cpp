163a6a33524cf571b16a7c2e13aac0be52c2c142
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-02 11:33:12 +0530

    Time: 5 ms (95.22%), Space: 33.1 MB (40.06%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -4,16 +4,15 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, 
-        greater<pair<int, pair<int, int>>>>pq;
+        deque<pair<int, pair<int, int>>>dq;
           
         int drow[4] = {1, 0, -1, 0};
         int dcol[4] = {0, 1, 0, -1};
        dist[0][0] = grid[0][0];
-       pq.push({grid[0][0], {0, 0}});
-       while(!pq.empty()){
-         auto it = pq.top();
-         pq.pop();
+       dq.push_front({grid[0][0], {0, 0}});
+       while(!dq.empty()){
+         auto it = dq.front();
+         dq.pop_front();
          int dis = it.first;
          int row = it.second.first;
          int col = it.second.second;
@@ -24,7 +23,8 @@ public:
             if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
                  if(dist[nrow][ncol] > dis + grid[nrow][ncol]){
                     dist[nrow][ncol] = dis + grid[nrow][ncol];
-                    pq.push({dist[nrow][ncol], {nrow, ncol}});
+                   if(grid[nrow][ncol] == 0)dq.push_front({dist[nrow][ncol], {nrow, ncol}});
+                   else dq.push_back({dist[nrow][ncol], {nrow, ncol}});
                  }
             }
          }
