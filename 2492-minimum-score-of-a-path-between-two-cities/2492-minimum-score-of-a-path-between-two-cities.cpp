8a22b3107024e255e218a44e1ba5d5654e4afac2
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-04 09:09:42 +0530

    Time: 15 ms (92.49%), Space: 112 MB (89.46%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,27 +1,44 @@
+class DisJointSet{
+    public:
+    vector<int>parent, size;
+    DisJointSet(int n){
+     parent.resize(n+1, 0);
+     size.resize(n+1, 0);
+     for(int i = 0; i <= n; i++)parent[i] = i;
+    }
+
+    int find(int node){
+        if(parent[node] == node)return node;
+        return parent[node] = find(parent[node]);
+    }
+    void unionbysize(int u, int v){
+        int ulp_u = find(u);
+        int ulp_v = find(v);
+        if(ulp_u == ulp_v)return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        for (auto& it : roads) {
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
-        }
-        queue<int> q;
-        vector<int> vis(n + 1, 0);
-        vis[1] = 1;
-        q.push(1);
+        DisJointSet ds(n);
         int mini = INT_MAX;
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            vis[node] = 1;
-            for (auto& it : adj[node]) {
-            mini = min(mini, it.second);
-                if (!vis[it.first]) {
-                    q.push(it.first);
-                }
-            }
+        for (auto& it : roads) {
+          ds.unionbysize(it[0], it[1]);
         }
-        return mini;
+        
+        for(auto& it: roads){
+            if(ds.find(it[0]) == ds.find(1))mini = min(mini, it[2]);
+        }
+       return mini;
     }
 };
