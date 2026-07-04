f5dc20670e5d7250499299774b536c47243b2a31
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-04 08:55:42 +0530

    Time: 104 ms (29.88%), Space: 136.3 MB (46.29%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-       vector<vector<pair<int, int>>>adj(n+1);
-       for(auto &it: roads){
-        adj[it[0]].push_back({it[1], it[2]});
-        adj[it[1]].push_back({it[0], it[2]});
-       }
-       queue<int>q;
-       vector<int>vis(n+1, 0);
-       vis[1] = 1;
-       q.push(1);
-       int mini = INT_MAX;
-       while(!q.empty()){
-        int node = q.front();
-        q.pop();
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto& it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        queue<int> q;
+        vector<int> vis(n + 1, 0);
+        vis[1] = 1;
+        q.push(1);
+        int mini = INT_MAX;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
             vis[node] = 1;
-        for(auto &it: adj[node]){
-            if(!vis[it.first]){
+            for (auto& it : adj[node]) {
             mini = min(mini, it.second);
-            q.push(it.first);
+                if (!vis[it.first]) {
+                    q.push(it.first);
+                }
             }
         }
-       }
-       return mini;
+        return mini;
     }
 };
