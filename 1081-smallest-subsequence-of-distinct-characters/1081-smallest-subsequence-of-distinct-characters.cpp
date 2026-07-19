ed99af37f3d8bf5ae88c255cdece383fac96221b
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-19 19:12:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (79.99%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -4,7 +4,7 @@ public:
       vector<int>alpha(26, 0);
       for(int i = 0; i < s.size(); i++)alpha[s[i] - 'a'] = i;
       vector<bool>v(26, false);
-        string st;
+        string st = "";
         for (int i = 0; i < s.size(); i++) {
             if (v[s[i] - 'a'])continue;
             while (!st.empty() && st.back() > s[i] && alpha[st.back() - 'a'] > i) {
