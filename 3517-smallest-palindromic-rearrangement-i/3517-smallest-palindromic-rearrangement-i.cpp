89ff6147ee6bcb0a4caaf9b8c39037d7dfbee94b
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-28 15:01:55 +0530

    Time: 12 ms (97.58%), Space: 54 MB (98.46%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -4,13 +4,14 @@ public:
         int n = s.size(), j = 0;
         vector<int>freq(26, 0);
 
-        for (int i = 0; i < n >> 1; i++)            
+        for (int i = 0; i < n/2; i++)            
             freq[s[i] - 'a']++;    
 
         for (int i = 0; i < 26; i++) {
             while (freq[i]--) {                
                 s[j] = 'a' + i;                      
-                s[n - 1 - j++] = 'a' + i; 
+                s[n - 1 - j] = 'a' + i;
+                j++; 
             }
         }
 
