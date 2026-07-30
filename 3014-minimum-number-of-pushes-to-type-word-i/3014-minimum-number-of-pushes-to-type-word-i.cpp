fc5468565fe2c910cc64feb9443bfe4692180ec9
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-30 12:50:26 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -6,9 +6,12 @@ public:
         sort(v.begin(), v.end());
         int count = 0, ans = 0, value = 1;
         for(int i = 25; i >= 0; i--){
-           ans += value*v[i];
+          if(v[i] > 0){
+             ans += value*v[i];
            count++;
            if(count % 8 == 0)value++;
+        }
+        else break;
         }
         return ans;
     }
