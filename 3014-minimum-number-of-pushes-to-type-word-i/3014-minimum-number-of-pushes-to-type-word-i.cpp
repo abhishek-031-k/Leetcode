635810a8b818fbcd8c983784f0acb6a42fcec064
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-30 12:49:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.81%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>v(26, 0);
+        for(auto &it : word)v[it - 'a']++;
+        sort(v.begin(), v.end());
+        int count = 0, ans = 0, value = 1;
+        for(int i = 25; i >= 0; i--){
+           ans += value*v[i];
+           count++;
+           if(count % 8 == 0)value++;
+        }
+        return ans;
+    }
+};
