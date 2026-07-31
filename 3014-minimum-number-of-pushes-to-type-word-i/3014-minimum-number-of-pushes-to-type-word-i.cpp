8287352384bd19c9d3cc21b7926be7c474ed2919
Abhishek kumar <abhishkumar159@gmail.com>
2026-07-31 09:59:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (27.42%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-       map<char, int>mpp;
-        for(auto &it : word)mpp[it]++;
+      vector<int>v(26, 0);
+        for(auto &it : word)v[it - 'a']++;
         
         int count = 0, ans = 0, value = 1;
-        for(auto &it : mpp){
-             ans += value * it.second;
-           count++;
+        for(int i = 0; i < 26; i++){
+           if(v[i] > 0){  ans += value;
+             count++;
            if(count % 8 == 0)value++;
+        }
         }
         return ans;
     }
