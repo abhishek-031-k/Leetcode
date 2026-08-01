2607de3de807244496a465b9f45a460faf43d349
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-01 10:09:24 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -4,12 +4,13 @@ public:
     int solve(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
        if(i == j)return nums[i];
        if(dp[i][j] != -1)return dp[i][j];
-       return dp[i][j] = max(nums[i] - solve(i+1, j, nums, dp), nums[j] - solve(i, j-1, nums, dp));
+        int leftpick = nums[i] - solve(i+1, j, nums, dp);
+        int rightpick =  nums[j] - solve(i, j-1, nums, dp);
+       return dp[i][j] = max(leftpick, rightpick);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int totalsum = accumulate(nums.begin(), nums.end(), 0);
         vector<vector<int>>dp(n, vector<int>(n, -1));
         int sum = solve(0, n-1, nums, dp);
         return sum >= 0 ? true : false;
