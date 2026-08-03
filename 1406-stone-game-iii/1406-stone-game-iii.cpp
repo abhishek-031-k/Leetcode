db566d0249950bc0a7cd5f866d1b4e518bacd065
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-03 11:07:35 +0530

    Time: 15 ms (82.12%), Space: 136 MB (88.2%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int solve(int ind, vector<int>& nums, vector<int>& dp) {
-        int n = nums.size();
-        if (ind >= n) return 0;
-        if (dp[ind] != -1) return dp[ind];
-        int take = 0;
+    int solve(int n, vector<int>& nums) {
+        vector<int> dp(n + 1, 0);
         int ans = INT_MIN;
-        for (int i = ind; i < min(n, ind + 3); i++) {
-            take += nums[i];
-            ans = max(ans, take - solve(i + 1, nums, dp));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int take = 0, ans = INT_MIN;
+            for (int i = ind; i < min(n, ind + 3); i++) {
+                take += nums[i];
+                 ans = max(ans, take - dp[i + 1]);
+            }
+            dp[ind] = ans;
         }
-        return dp[ind] = ans;
+        return dp[0];
     }
 
     string stoneGameIII(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        int sum = solve(0, nums, dp);
-        if (sum == 0) return "Tie";
-        if (sum > 0) return "Alice";
+        int sum = solve(n, nums);
+        if (sum == 0)
+            return "Tie";
+        if (sum > 0)
+            return "Alice";
         return "Bob";
     }
 };
