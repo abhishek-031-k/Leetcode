16e1be9b25834fd94876cb5f45011d7d3840fbf1
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-09 22:11:07 +0530

    Time: 6 ms (80.94%), Space: 13.8 MB (35.31%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-
-//    int solve(int i, int m, int n, vector<int>&piles){
-         
-//    }
+    int solve(int i, int m, vector<int>& piles, vector<int>& suff, vector<vector<int>>& dp) {
+        int n = piles.size();
+        if(i >= n) return 0;
+        if(dp[i][m] != -1) return dp[i][m];
+        int ans = 0;
+         for(int x = 1; x <= 2*m && i+x <= n; x++) {
+            int next = solve(i+x, max(m, x), piles, suff, dp);
+            ans = max(ans, suff[i] - next);
+        }
+        return dp[i][m] = ans;
+    }
 
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<int> arr(1);
-        for(int i = 0; i<n; i++) arr.push_back(piles[i]);
-        vector<int> prefSum(n+1);
-
-        prefSum[0] = 0;
-        for(int i = 1; i<=n; i++) prefSum[i] = prefSum[i-1] + arr[i];
-        int dp[n+2][n+2];
-        memset(dp, -1, sizeof(dp));
-        auto findAns = [&](auto &self, int l, int r) -> int{
-            if(l>=n+1) return 0;
-            if(dp[l][r]+1) return dp[l][r];
-            int range = r - l + 1;
-            int ttl = 0;
-            int nextSum;
-            int ans = -1000000000;
-            int cnt = 0;
-            for(int i = l; i<n+1&&i<=r; i++){
-                cnt++;
-                ttl += arr[i];
-                nextSum = prefSum[n] - prefSum[i];
-                ans = max(ans, ttl + nextSum - self(self, i+1, min(i+max(range, 2*cnt), n)));
-            }
-            return dp[l][r] = ans;
-        };
-
-        return findAns(findAns, 1, 2);
+        vector<int> suff(n+1, 0);
+        for(int i = n-1; i >= 0; i--)suff[i] = suff[i+1] + piles[i];
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return solve(0, 1, piles, suff, dp);
     }
 };
