1d785403e751afb129a2ab01cfd1f3c89a7367dc
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-13 23:26:22 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.93%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,7 +9,7 @@ public:
         while(low <= high){
             int mid = low + (high - low)/2;
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])return mid;
-            if(nums[mid] > nums[mid-1])low = mid+1;
+           else if(nums[mid] > nums[mid-1])low = mid+1;
             else high = mid-1;
         }
         return -1;
