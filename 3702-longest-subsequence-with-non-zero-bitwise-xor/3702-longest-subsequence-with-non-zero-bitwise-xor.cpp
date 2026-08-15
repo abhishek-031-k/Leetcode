7e5433caec869e06b46b6b14fb8915b9a5a0d160
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-15 10:51:31 +0530

    Time: 0 ms (100%), Space: 171.2 MB (56.25%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
         int n = nums.size();
-        int count = 0, value = nums[0];
+        int value = nums[0], count = 0;
         for(int i = 1; i < n; i++){
             if(value == 0)count++;
-            value = value ^ nums[i];
+            value ^= nums[i];
         }
-        if(value == 0 && count == n-1)return 0;
-        else if(value == 0)return n-1;
-        return n;
+        if(value != 0)return n;
+        if(count == n-1)return 0;
+        return n-1;
     }
 };
