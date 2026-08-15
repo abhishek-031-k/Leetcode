074357a91e4f27a20978dd1cb852e49587500600
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-15 14:27:09 +0530

    Time: 3 ms (86.15%), Space: 22.2 MB (88.58%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
    int drow[4] = {1, 0, -1, 0};
    int dcol[4] = {0, 1, 0, -1};
 
@@ -9,17 +8,14 @@ public:
         if(height > heights[row][col] || vis[row][col])return;
         vis[row][col] = true;
         for(int i = 0; i < 4; i++){
-         int nrow = drow[i] + row;
-         int ncol = dcol[i] + col;
+         int nrow = drow[i] + row, ncol = dcol[i] + col;
          dfs(nrow, ncol, n, m, heights[row][col], heights, vis);
         }
    }
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int n = heights.size();
-        int m = heights[0].size();
-        vector<vector<bool>>visp(n ,vector<bool>(m, false));
-        vector<vector<bool>>visa(n ,vector<bool>(m, false));
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<bool>>visp(n ,vector<bool>(m, false)), visa(n ,vector<bool>(m, false));
 
         for(int i = 0; i < n; i++){
             dfs(i, 0, n, m, INT_MIN, heights, visp);
