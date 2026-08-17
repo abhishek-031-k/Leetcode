bf951b9e31e6358d9d418f798e1abb1cde1c2553
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-17 15:50:27 +0530

    Time: 3 ms (32.77%), Space: 69.6 MB (67.6%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,12 +5,12 @@ public:
        int zeros = 0, r = 0, l = 0;
        while(r < n){
         if(nums[r] == 0)zeros++;
-        while(zeros > k){
+       if(zeros > k){
         if(nums[l] == 0)zeros--;
-            l++;
-        }
-        maxlen = max(maxlen, r-l+1);
-        r++;
+        l++;
+       }
+       if(zeros <= k)maxlen = max(maxlen, r-l+1);
+       r++;
        }
        return maxlen;
     }
