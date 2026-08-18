0285b22d1dea09f0f701eb919e5299f51bb7cf20
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-18 20:19:58 +0530

    Time: 0 ms (100%), Space: 46.2 MB (64.29%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -5,8 +5,7 @@ public:
       int lsum = 0, rsum = 0, ans = 0;
       for(int i = 0; i < k; i++)lsum += cardPoints[i];
          ans = lsum;
-       
-       int rightind = n-1;
+
       for(int i = k-1; i >= 0; i--){
            lsum -= cardPoints[i];
            rsum += cardPoints[n-k+i];
