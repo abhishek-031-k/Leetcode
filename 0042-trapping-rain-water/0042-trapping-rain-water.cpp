8485673eaff8b2344edbbe42aa82f6a42e88ef83
Abhishek kumar <abhishkumar159@gmail.com>
2026-08-24 19:52:08 +0530

    Time: 0 ms (100%), Space: 27.4 MB (6.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-
-    void left(vector<int>&height, vector<int>&leftmax){
-      leftmax[0] = height[0];
-      for(int i = 1; i < height.size(); i++){
-        leftmax[i] = max(leftmax[i-1], height[i]);
-      }
-    }
-    
-    void right(vector<int>&height, vector<int>&rightmax){
-        int n = height.size();
-        rightmax[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--){
-          rightmax[i] = max(rightmax[i+1], height[i]);
-        }
-    }
-
     int trap(vector<int>& height) {
-      int n = height.size();
-      vector<int>leftmax(n), rightmax(n);
-      left(height, leftmax);
-      right(height, rightmax);
-      int ans = 0;
-      for(int i = 0; i < n; i++){
-        ans += min(leftmax[i], rightmax[i]) - height[i];
-      }
-      return ans;
+        int n = height.size();
+        vector<int> pre(n, height[0]), suff(n, height[n - 1]);
+        for (int i = 1; i < n; i++)pre[i] = max(height[i], pre[i - 1]);
+        for (int i = n - 2; i >= 0; i--)suff[i] = max(height[i], suff[i + 1]);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += min(pre[i], suff[i]) - height[i];
+        }
+        return ans;
     }
 };
